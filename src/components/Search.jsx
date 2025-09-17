81845084a98178bb5b24ba9c29d93bbede5199ea
BRajendra10 <rajendrabehera8116@gmail.com>
2025-09-17 20:19:00 +0530

    implement searching feature for users

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LuSearch } from "react-icons/lu";
 
 function Search() {
+    const [query, setQuery] = useState("");
+    console.log(query);
+    
     return (
         <div className="min-w-[15rem] w-[25rem] h-fit grid grid-cols-12 bg-white shadow-lg rounded-xl py-1 px-3">
             <div className="col-span-1 flex justify-start items-center">
                 <LuSearch className="text-xl" />
             </div>
-            <input className="col-span-11 w-full outline-none rounded-sm py-2 px-3" type="text" name="search-chats" id="search-chats" placeholder="Search chats" />
+            <input className="col-span-11 w-full outline-none rounded-sm py-2 px-3" 
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            type="text" name="search-chats" id="search-chats" placeholder="Search chats" />
         </div>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from "react-redux";
+import { LuSearch } from "react-icons/lu";
 import Chat from "./Chat";
-import Search from "./Search";
 
 function Sidebar() {
+    const [query, setQuery] = useState("");
+    const [result, setResult] = useState([]);
     const { users, currentUser } = useSelector((state) => state.users);
+
+    useEffect(() => {
+        if (query) {
+            const results = users.filter((user) => user.displayName.toLowerCase().includes(query.toLowerCase()));
+            setResult(results);
+        }
+    }, [query, users])
 
     return (
         <div className="w-[22rem] bg-zinc-100 border rounded-lg flex flex-col">
             {/* Search */}
-            <div className="h-[4rem] p-2 flex items-center border-gray-200">
-                <Search />
+            <div className="w-full h-[4.5rem] flex justify-center items-center bg-none p-2">
+                <div className="w-[20rem] h-[3.3rem] bg-white rounded-lg shadow-lg p-2 flex">
+                    <div className="col-span-1 flex justify-start items-center">
+                        <LuSearch className="text-xl" />
+                    </div>
+                    <input className="col-span-11 w-full outline-none rounded-sm py-2 px-3"
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        type="text" name="search-chats" id="search-chats" placeholder="Search chats" />
+                </div>
             </div>
 
             {/* Chat list */}
             <div className="flex-1 overflow-y-auto p-2 space-y-2">
-                {users.map((el) => (
+                {result.length > 0 ? result.map((el) => (
+                    el.uid !== currentUser.uid && <Chat
+                        key={el.id}
+                        user={{
+                            avatar: el.photoURL,
+                            name: el.displayName,
+                            lastMessage: "Hey!",
+                        }}
+                    />
+                )) : users.map((el) => (
                     el.uid !== currentUser.uid && <Chat
                         key={el.id}
                         user={{
