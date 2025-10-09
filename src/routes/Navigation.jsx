3722cea35b14e0ff20f5e2a370c12030d8ebc4ba
BRajendra10 <rajendrabehera8116@gmail.com>
2025-10-09 20:18:44 +0530

    navigation issues in small screen sovled

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
 
 import Signin from '../screens/Signin';
@@ -11,12 +11,14 @@ import ChatsContainer from '../components/ChatsContainer';
 function Navigation() {
     const currentUser = useSelector((state) => state.users.currentUser);
     const selectedUser = useSelector((state) => state.users.selectedUser);
-    
+
     return (
         <Routes>
             <Route path="/" element={<Home />} >
                 <Route index element={<Sidebar />} />
-                <Route path="/chats" element={<ChatsContainer currentUser={currentUser} selectedUser={selectedUser} />} />
+                <Route path="/chats" element={!selectedUser.uid ?
+                    (<Navigate to="/" replace />)
+                    : (<ChatsContainer currentUser={currentUser} selectedUser={selectedUser} />)} />
             </Route>
             <Route path="/login" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
